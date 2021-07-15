9c894fcf09da51fa8729e696a58f04143a929ca6
AJ <79541631+TheAJManagement@users.noreply.github.com>
2021-07-15 21:59:06 +0100

    Update Separator.js

--- src/components/Separator.js
+++ src/components/Separator.js
@@ -1,22 +1,27 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import ScrollAnimation from "react-animate-on-scroll";
 
-function Separator({textSide, title, text, btnLink, btnText, imgSrc}) {
-	return textSide === 'left' ? (
+function Separator({ textSide, title, text, btnLink, btnText, imgSrc }) {
+	return textSide === "left" ? (
 		<section className="separator">
-			<div className="container grid-container">
-				<article className="separator-text">
-					<h2>{title}</h2>
-					<p>{text}</p>
-					<div className="separator-link">
-						<Link to={btnLink} className="btn">{btnText}</Link>
-					</div>
-				</article>
-				<figure className="separator-figure flex-container">
-					<img className="separator-img" src={imgSrc} alt=""/>
-				</figure>
-			</div>	
+			<ScrollAnimation animateIn="animate__bounceIn" animateOnce={true}>
+				<div className="container grid-container">
+					<article className="separator-text">
+						<h2>{title}</h2>
+						<p>{text}</p>
+						<div className="separator-link">
+							<Link to={btnLink} className="btn">
+								{btnText}
+							</Link>
+						</div>
+					</article>
+					<figure className="separator-figure flex-container">
+						<img className="separator-img" src={imgSrc} alt="" />
+					</figure>
+				</div>
+			</ScrollAnimation>
 		</section>
-	) : ( 
+	) : (
 		<section className="separator">
 			<div className="container grid-container">
 				<figure className="separator-figure flex-container">
@@ -26,12 +31,14 @@ function Separator({textSide, title, text, btnLink, btnText, imgSrc}) {
 					<h2>{title}</h2>
 					<p>{text}</p>
 					<div className="separator-link">
-						<Link to={btnLink} className="btn">{btnText}</Link>
+						<Link to={btnLink} className="btn">
+							{btnText}
+						</Link>
 					</div>
 				</article>
-			</div>	
-		</section> 
-	)
+			</div>
+		</section>
+	);
 }
 
 export default Separator;
