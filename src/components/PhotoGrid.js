97fcdaba7a22fc20d8feb38c84f3d170cc009152
AJ <79541631+TheAJManagement@users.noreply.github.com>
2021-07-15 21:59:03 +0100

    Create PhotoGrid.js

---
+++ src/components/PhotoGrid.js
@@ -0,0 +1,65 @@
+import ScrollAnimation from "react-animate-on-scroll";
+import { Link } from "react-router-dom";
+
+function PhotoGrid({ items }) {
+	return (
+		<section className="menu-display">
+			<ScrollAnimation animateIn="animate__rollIn" animateOnce={true}>
+				<h2>What we serve</h2>
+			</ScrollAnimation>
+			<div className="container photo-grid">
+				{items.map((item, idx) =>
+					idx % 2 === 0 ? (
+						<ScrollAnimation
+							key={idx}
+							animateIn="animate__bounceInLeft"
+							animateOnce={true}
+						>
+							<article className="photo-grid-content">
+								<figure className="grid-img-container align-start">
+									<img className="photo-grid-img" src={item.img} alt="Menu" />
+								</figure>
+								<div key={item.title} className="photo-grid-text">
+									<h3>{item.title}</h3>
+									<h4>
+										Starting from{" "}
+										<span className="gold-highlight">${item.pricing}</span>
+									</h4>
+									<p>{item.text}</p>
+									<Link to="/menu" className="btn">
+										View Menu
+									</Link>
+								</div>
+							</article>
+						</ScrollAnimation>
+					) : (
+						<ScrollAnimation
+							key={idx}
+							animateIn="animate__bounceInRight"
+							animateOnce={true}
+						>
+							<article className="photo-grid-content">
+								<div key={item.title} className="photo-grid-text">
+									<h3>{item.title}</h3>
+									<h4>
+										Starting from{" "}
+										<span className="gold-highlight">${item.pricing}</span>
+									</h4>
+									<p>{item.text}</p>
+									<Link to="/menu" className="btn">
+										View Menu
+									</Link>
+								</div>
+								<figure className="grid-img-container align-end">
+									<img className="photo-grid-img" src={item.img} alt="Menu" />
+								</figure>
+							</article>
+						</ScrollAnimation>
+					)
+				)}
+			</div>
+		</section>
+	);
+}
+
+export default PhotoGrid;
