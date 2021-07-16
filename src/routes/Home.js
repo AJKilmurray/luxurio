e3df68b74c3883ebc3ce98b7ddb5c73f2ff9d516
AJ <79541631+TheAJManagement@users.noreply.github.com>
2021-07-16 23:35:02 +0100

    Update Home.js

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,23 +1,22 @@
+import { Link } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
 import Separator from "../components/Separator";
 import stockMenu from "../images/stock-menu.png";
 import PhotoGrid from "../components/PhotoGrid";
+import Footer from "../components/Footer";
 import Breakfast from "../images/breakfast.jpg";
 import Lunch from "../images/lunch.jpg";
 import Dinner from "../images/dinner.jpg";
 import Dessert from "../images/dessert.jpg";
-import Footer from "../components/Footer";
 
 function Home() {
 	// Hero Title & Text
-	const heroText =
-		"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vitae posuere augue. Nunc maximus eros eget elit iaculis, et facilisis metus porta. In hac habitasse platea dictumst. Phasellus varius tempus ipsum a egestas. Phasellus ornare, dolor sed molestie euismod, felis diam consequat sapien, sit amet rutrum justo tellus nec velit. Nulla vitae ornare dolor. Morbi imperdiet tellus ex.";
+	const heroText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vitae posuere augue. Nunc maximus eros eget elit iaculis, et facilisis metus porta. In hac habitasse platea dictumst. Phasellus varius tempus ipsum a egestas. Phasellus ornare, dolor sed molestie euismod, felis diam consequat sapien, sit amet rutrum justo tellus nec velit. Nulla vitae ornare dolor. Morbi imperdiet tellus ex.";
 	const heroTitle = "Exceptional Tastes";
 
 	// Separator Title & Text
-	const sepText =
-		"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vitae posuere augue. Nunc maximus eros eget elit iaculis, et facilisis metus porta. In hac habitasse platea dictumst. Phasellus varius tempus ipsum a egestas. Phasellus ornare, dolor sed molestie euismod, felis diam consequat sapien, sit amet rutrum justo tellus nec velit. Nulla vitae ornare dolor. Morbi imperdiet tellus ex.";
+	const sepText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vitae posuere augue. Nunc maximus eros eget elit iaculis, et facilisis metus porta. In hac habitasse platea dictumst. Phasellus varius tempus ipsum a egestas. Phasellus ornare, dolor sed molestie euismod, felis diam consequat sapien, sit amet rutrum justo tellus nec velit. Nulla vitae ornare dolor. Morbi imperdiet tellus ex.";
 	const sepTitle = "Extravagant Menus";
 
 	// Photo Grid Items
@@ -48,11 +47,23 @@ function Home() {
 		},
 	];
 
-	// Accordians
-	const accordians = [
+	// Footer Accordians
+	const footerAccordians = [
 		{
-			title: "t",
-			text: "text",
+			title: "Links",
+			list: [<Link to="/">Link1</Link>, <Link to="/">Link2</Link>, <Link to="/">Link3</Link>, <Link to="/">Link4</Link>, <Link to="/">Link5</Link>],
+		},
+		{
+			title: "Help",
+			list: [<Link to="/">Link1</Link>, <Link to="/">Link2</Link>, <Link to="/">Link3</Link>, <Link to="/">Link4</Link>, <Link to="/">Link5</Link>],
+		},
+		{
+			title: "Links",
+			list: [<Link to="/">Link1</Link>, <Link to="/">Link2</Link>, <Link to="/">Link3</Link>, <Link to="/">Link4</Link>, <Link to="/">Link5</Link>],
+		},
+		{
+			title: "Help",
+			list: [<Link to="/">Link1</Link>, <Link to="/">Link2</Link>, <Link to="/">Link3</Link>, <Link to="/">Link4</Link>, <Link to="/">Link5</Link>],
 		},
 	];
 
@@ -60,23 +71,11 @@ function Home() {
 		<main>
 			<section id="welcome" className="landing-section">
 				<Navbar navLinks={["menu", "bookings", "about"]} />
-				<Hero
-					title={heroTitle}
-					text={heroText}
-					buttonLink="bookings"
-					buttonText="Book Now"
-				/>
+				<Hero title={heroTitle} text={heroText} buttonLink="bookings" buttonText="Book Now" />
 			</section>
-			<Separator
-				textSide="left"
-				title={sepTitle}
-				text={sepText}
-				btnLink="menu"
-				btnText="Menu"
-				imgSrc={stockMenu}
-			/>
+			<Separator textSide="left" title={sepTitle} text={sepText} btnLink="menu" btnText="Menu" imgSrc={stockMenu} />
 			<PhotoGrid items={photoGridItems} />
-			<Footer />
+			<Footer accordians={footerAccordians} />
 		</main>
 	);
 }
