9e59efdfb7913ed012e6e7fe447c4b416927d94b
AJ <79541631+TheAJManagement@users.noreply.github.com>
2021-07-16 23:34:42 +0100

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,31 +1,16 @@
-import { FaChevronDown } from "react-icons/fa";
+import ScrollAnimation from "react-animate-on-scroll";
+import FooterAccordian from "./FooterAccordian";
 
 function Footer({ accordians }) {
 	return (
 		<footer>
-			<div className="container grid-container accordian-container">
-				<article className="accordian">
-					<div className="accordian-header">
-						<h4>Title</h4>
-						<div className="toggle-accordian">
-							<FaChevronDown />
-						</div>
-					</div>
-					<div class="accordian-text-container">
-						<div class="accordian-text">
-							<p>
-								Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean
-								vitae posuere augue. Nunc maximus eros eget elit iaculis, et
-								facilisis metus porta. In hac habitasse platea dictumst.
-								Phasellus varius tempus ipsum a egestas. Phasellus ornare, dolor
-								sed molestie euismod, felis diam consequat sapien, sit amet
-								rutrum justo tellus nec velit. Nulla vitae ornare dolor. Morbi
-								imperdiet tellus ex.
-							</p>
-						</div>
-					</div>
-				</article>
-			</div>
+			<ScrollAnimation animateIn="animate__bounceInUp" animateOnce={true}>
+				<div className="container footer-container grid-container">
+					{accordians.map((accordian, idx) => (
+						<FooterAccordian key={idx} title={accordian.title} list={accordian.list} id={idx} />
+					))}
+				</div>
+			</ScrollAnimation>
 		</footer>
 	);
 }
