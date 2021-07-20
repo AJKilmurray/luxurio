69d0ea2f24ebf2f91344379339ec3173cca3e35e
AJ <79541631+TheAJManagement@users.noreply.github.com>
2021-07-20 16:21:59 +0100

    [PC] Completed category routing

--- src/components/MenuNav.js
+++ src/components/MenuNav.js
@@ -1,69 +1,57 @@
-import {
-  Link,
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  useParams,
-} from "react-router-dom";
+import { Link, BrowserRouter as Router, Switch, Route, useParams } from "react-router-dom";
 
 function MenuNav({ heading, navCategories }) {
-  const categories = Object.values(navCategories);
-  return (
-    <Router>
-      <nav className="menu-nav">
-        <div className="container menu-nav-container">
-          <div className="menu-nav-header">
-            <h3>{heading}</h3>
-          </div>
-          <ul className="menu-nav-links">
-            {categories.map((category, idx) => (
-              <Link
-                to={`/menu/category/${category.name}`}
-                className="menu-nav-link"
-                key={idx}
-              >
-                {category.name}
-              </Link>
-            ))}
-          </ul>
-        </div>
-        <Switch>
-          <Route
-            path="/menu/category/:id"
-            children={<DisplayID menuCategories={navCategories} />}
-          />
-        </Switch>
-      </nav>
-    </Router>
-  );
+	const categories = Object.values(navCategories);
+	return (
+		<Router>
+			<nav className="menu-nav">
+				<div className="container menu-nav-container">
+					<div className="menu-nav-header">
+						<h3>{heading}</h3>
+					</div>
+					<ul className="menu-nav-links">
+						{categories.map((category, idx) => (
+							<Link to={`/menu/category/${category.name}`} className="menu-nav-link" key={idx}>
+								{category.name}
+							</Link>
+						))}
+					</ul>
+				</div>
+				<Switch>
+					<Route path="/menu/category/:id" children={<DisplayID menuCategories={navCategories} />} />
+				</Switch>
+			</nav>
+		</Router>
+	);
 }
 
 function DisplayID({ menuCategories }) {
-  let { id } = useParams();
-  const categories = Object.values(menuCategories);
-  return (
-    <section className="menu-category-display">
-      <div className="container">
-        <Link to="/menu" className="menu-return btn">
-          Go Back
-        </Link>
-        <h3>Menu Items</h3>
-        {categories.map((category) => {
-          if (category.name === id) {
-            return category.menu.map((item, idx) => {
-              return (
-                <div className="menu-card" key={idx}>
-                  <h3>{item.title}</h3>
-                  <h4>{item.price}</h4>
-                  <p>{item.text}</p>
-                </div>
-              );
-            });
-          }
-        })}
-      </div>
-    </section>
-  );
+	let { id } = useParams();
+	const categories = Object.values(menuCategories);
+
+	return (
+		<section className="menu-category-display">
+			<div className="container">
+				<Link to="/menu" className="menu-return btn">
+					Go Back
+				</Link>
+				<h3 className="menu-category-title">{id} Menu</h3>
+				{categories.map((category) => {
+					if (category.name === id) {
+						return category.menu.map((item, idx) => {
+							return (
+								<div className="menu-card" key={idx}>
+									<h3>{item.title}</h3>
+									<h4>{item.price}</h4>
+									<p>{item.text}</p>
+								</div>
+							);
+						});
+					}
+				})}
+			</div>
+		</section>
+	);
 }
 
 export default MenuNav;
