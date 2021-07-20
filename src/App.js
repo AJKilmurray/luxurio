06642f6210d043288a57df99c7c811a099ea1828
AJ <79541631+AJKilmurray@users.noreply.github.com>
2021-07-20 14:33:34 +0100

    [Laptop] Menu categories with routing

--- src/App.js
+++ src/App.js
@@ -3,17 +3,20 @@ import Home from "./routes/Home";
 import Menu from "./routes/Menu";
 import About from "./routes/About";
 import Beans from "./images/beans.svg";
+import BackToTop from "./components/BackToTop";
 
 function App() {
-	return (
-		<Router>
-			<img src={Beans} className="beans" alt="" />
-			<Route path="/" exact component={Home} />
-			<Route path="/menu" exact component={Menu} />
-			<Route path="/bookings" exact />
-			<Route path="/about" exact component={About} />
-		</Router>
-	);
+  return (
+    <Router>
+      <img src={Beans} className="beans" alt="" />
+      <Route path="/" exact component={Home} />
+      <Route path="/menu" exact component={Menu} />
+      <Route path="/bookings" exact />
+      <Route path="/about" exact component={About} />
+      <Route path="/menu/category" component={Menu} />
+      <BackToTop />
+    </Router>
+  );
 }
 
 export default App;
--- src/routes/Menu.js
+++ src/routes/Menu.js
@@ -1,22 +1,24 @@
 import Navbar from "../components/Navbar";
 import MenuNav from "../components/MenuNav";
 import Footer from "../components/Footer";
-import BackToTop from "../components/BackToTop";
 
 // Utils
-import { footerAccordians, menuNavCategories, menuNavHeader } from "../utils/utils";
+import {
+  footerAccordians,
+  menuNavCategories,
+  menuNavHeader,
+} from "../utils/utils";
 
 function Menu() {
-	return (
-		<>
-			<section className="landing-section" id="menu-landing">
-				<Navbar navLinks={["/", "bookings", "about"]} />
-				<MenuNav heading={menuNavHeader} navCategories={menuNavCategories} />
-			</section>
-			<BackToTop />
-			<Footer accordians={footerAccordians} />
-		</>
-	);
+  return (
+    <>
+      <section className="landing-section" id="menu-landing">
+        <Navbar navLinks={["/", "bookings", "about"]} />
+        <MenuNav heading={menuNavHeader} navCategories={menuNavCategories} />
+      </section>
+      <Footer accordians={footerAccordians} />
+    </>
+  );
 }
 
 export default Menu;
